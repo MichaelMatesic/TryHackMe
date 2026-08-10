cd671dcca13de5c46510815a61ca5d2d105f85de
Michael <mrbmates@uwaterloo.ca>
2026-08-10 14:18:47 -0400

    Updates: - str check for task01.py.

--- scripting/task01/task01.py
+++ scripting/task01/task01.py
@@ -6,6 +6,6 @@ with open("b64_1550406728131.txt", "r") as f:
 for i in range(50):
     b64_encoded = base64.b64decode(b64_encoded)
 
-b64_decoded = b64_encoded.decode("utf-8")
+b64_decoded = b64_encoded if isinstance(b64_encoded, str) else b64_encoded.decode("utf-8")
 
 print(f"Decoded: {b64_decoded}")
