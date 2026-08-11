2d8fbb20cbb769b8ca95bd564e9eb30af1945176
Michael <mrbmates@uwaterloo.ca>
2026-08-11 11:41:21 -0400

    Updates: - crackthehash.sh challenge solutions and folder. - General workspace updates for individual venvs and automated selection.

---
+++ crackthehash/crackthehash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+src_dir="$HOME/Documents/TryHackMe/crackthehash/"
+
+while IFS=: read -r mode_number mode_name hash salt; do
+    salt=${salt%$'\r'}
+    if [[ -n "$salt" ]]; then
+        hash_input="$hash:$salt"
+    else
+        hash_input="$hash"
+    fi
+    echo -e "\nCracking $hash_input with mode $mode_number ($mode_name) ...\n";
+    ./hashcat -m "$mode_number" -a 0 -o "${src_dir}cracked.txt" "$hash_input" "${src_dir}rockyou.txt"
+done < "${src_dir}hashes.txt"
